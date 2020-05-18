5a1381bcad2f91ba37f9b5b137163476226e4507
caiyuan <caiyuan@live.com>
2020-05-18 14:09:54 +0800

    '通用提交'

--- javaagent/application/src/main/java/me/caiyuan/javaagent/Application.java
+++ javaagent/application/src/main/java/me/caiyuan/javaagent/Application.java
@@ -5,7 +5,10 @@ package me.caiyuan.javaagent;
  */
 public class Application {
 
+    static Welcome welcome = new Welcome();
+
     public static void main(String[] args) {
         System.out.println("Application.main()");
+        welcome.welcome("Tom");
     }
 }
---
+++ javaagent/application/src/main/java/me/caiyuan/javaagent/Welcome.java
@@ -0,0 +1,11 @@
+package me.caiyuan.javaagent;
+
+/**
+ * @author Ryan
+ */
+public class Welcome {
+
+    public void welcome(String name) {
+        System.out.println("Welcome, " + name);
+    }
+}
