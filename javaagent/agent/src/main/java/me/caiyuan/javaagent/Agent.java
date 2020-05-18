cb413f7ab4649eba888eacbd3b9a5bcfbfafc382
caiyuan <caiyuan@live.com>
2020-05-18 14:06:36 +0800

    '通用提交'

--- javaagent/agent/src/main/java/me/caiyuan/javaagent/Agent.java
+++ javaagent/agent/src/main/java/me/caiyuan/javaagent/Agent.java
@@ -1,6 +1,9 @@
 package me.caiyuan.javaagent;
 
+import java.lang.instrument.ClassFileTransformer;
+import java.lang.instrument.IllegalClassFormatException;
 import java.lang.instrument.Instrumentation;
+import java.security.ProtectionDomain;
 
 /**
  * @author Ryan
@@ -9,9 +12,12 @@ public class Agent {
 
     public static void premain(String args, Instrumentation instrumentation) {
         System.out.println("Agent.premain()");
-    }
-
-    public static void agentmain(String args, Instrumentation instrumentation) {
-        System.out.println("Agent.agentmain()");
+        instrumentation.addTransformer(new ClassFileTransformer() {
+            @Override
+            public byte[] transform(ClassLoader loader, String className, Class<?> classBeingRedefined, ProtectionDomain protectionDomain, byte[] classfileBuffer) throws IllegalClassFormatException {
+                System.out.println(loader.getClass() + " <- " + className);
+                return classfileBuffer;
+            }
+        });
     }
 }
